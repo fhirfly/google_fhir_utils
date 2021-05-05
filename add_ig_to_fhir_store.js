6c2189baaa4c6ea8ca716127db7089527a5000ae
FHIRFLY <richard.braman@fhirfly.io>
2021-05-05 12:35:10 -0400

    initial files, add ig to fhirstore ready for testing

--- add_ig_to_fhir_store.js
+++ add_ig_to_fhir_store.js
@@ -1,1 +1,80 @@
 //Node Module to Add ImplementationGuide to FHIR Store
+const fs = require('fs');
+const IG_DIRECTORY = "C:\\Users\\FHIRFLY\\Downloads\\full-ig\\site\\";
+const {google} = require('googleapis');
+const healthcare = google.healthcare('v1');
+
+const cloudRegion = 'us-central1';
+const projectId = 'fhirfly';
+const datasetId = 'synthea';
+const fhirStoreId = 'carin-bb';
+const bFormatNDJSON = false;
+
+async function installIGtoFHIRStore(){
+    var FhirIGBundle = createBundle();
+    fs.readdir(IG_DIRECTORY, function(err, filenames) {
+        if (err) {
+        console.log ("Error reading data directory")
+        return;
+        }
+        filenames.forEach(function(filename) {
+            if ((filename.startsWith("Structure") || filename.startsWith("ValueSet") || filename.startsWith("CodeSystem")  || filename.startsWith("CapabilityStatement") || filename.startsWith("ImplementationGuide") || filename.startsWith("ImplementationGuide")) && filename.endsWith(".json")){
+                try {
+                    var content = fs.readFileSync(IG_DIRECTORY + filename);
+                    var content = JSON.parse(content.toString());
+                    addResourceToBundle(FhirIGBundle, content);
+                    console.log("Added Resource to Bundle");
+                }
+                catch(err){
+                    console.log("Error adding resource to bundle");
+                }
+            }
+        });
+        const returnBundle = createFhirResource("Bundle", FhirIGBundle);
+        console.log(returnBundle);
+    });
+}
+
+function createBundle(){
+    bundle = {
+        "resourceType": "Bundle",
+        "type": "transaction",
+        "entry": []
+    }
+    return bundle;
+}
+
+function addResourceToBundle(bundle, resource){
+    resourceCount = bundle.entry.length;
+    bundle.entry[resourceCount] = resource;
+}
+
+
+async function createFhirResource(resourceType, body) {
+  const auth = await google.auth.getClient({
+    scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+  });
+
+  // Replace the following body with the data for the resource you want to
+  // create.
+  google.options({auth, headers: {'Content-Type': 'application/fhir+json'}});
+
+  const parent = `projects/${projectId}/locations/${cloudRegion}/datasets/${datasetId}/fhirStores/${fhirStoreId}`;
+
+  const request = {parent, type: resourceType, requestBody: JSON.stringify(body)};
+  const resource = await healthcare.projects.locations.datasets.fhirStores.fhir.create(
+    request
+  );
+  console.log(`Created FHIR resource with ID ${resource.data.id}`);
+  console.log(resource.data);
+  return;
+}
+
+try{
+    installIGtoFHIRStore();
+    return;
+}
+catch(err){
+    console.log(err);
+return;
+}
